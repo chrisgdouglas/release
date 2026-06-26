857134452b1b259290d2b99397a29ec8c66b5efb
Chris Douglas <christopher.douglas@concentrix.com>
2026-06-26 09:29:54 -0500

    Add QR code functionality.

---
+++ creds.php
@@ -0,0 +1,7 @@
+<?php
+
+$smtp_server = "smtp.zoho.com";
+$smtp_username = "folkfestphoto@chrisdouglas.dev";
+$smtp_password = "XLZ7yhuwzMAV#PfKk57soiySFm9z4T";
+$sender_name = "FolkFest Photo";
+$subject = "Winnipeg Folk Festival Photo Release";
